dbd2ce565ad2b11a9db8a0cb6fe1c4ae72bfc2b6
dgason <dave.ason@gmail.com>
2015-01-11 10:13:50 -0500

    final version

--- plot3.R
+++ plot3.R
@@ -14,10 +14,13 @@ mainData$DateTime <- strptime(mainData$DateTime, format = "%d/%m/%Y %H:%M:%S")
 filteredData <- subset(mainData, mainData$DateTime > ("2007-02-01 00:00:00") & mainData$DateTime < ("2007-02-03 00:00:00") )
 
 #generate the plot
-plot(filteredData$DateTime, filteredData$Sub_metering_1,type="l", col = "black", bg = "white", ylab="Energy sub metering", xlab= "")
-lines(filteredData$DateTime, filteredData$Sub_metering_2,type="l", col = "red", bg = "white", ylab="Energy sub metering", xlab="")
-lines(filteredData$DateTime, filteredData$Sub_metering_3,type="l", col = "blue", bg = "white", ylab="Energy sub metering", xlab= "")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
+with(filteredData, {
+  plot(DateTime, Sub_metering_1,type="l", col = "black", bg = "white", ylab="Energy sub metering", xlab= "")
+  lines(DateTime, Sub_metering_2,type="l", col = "red", bg = "white", ylab="Energy sub metering", xlab="")
+  lines(DateTime, Sub_metering_3,type="l", col = "blue", bg = "white", ylab="Energy sub metering", xlab= "")
+  legend("topright", xjust = 0, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
+
+})
 
 #save plot
 dev.copy(png, filename = "plot3.png", width = 480, height = 480, units = "px")
--- plot4.R
+++ plot4.R
@@ -1,42 +1,47 @@
-#########################################
+##################################################
 #plot4.R
-# generate the combined 2x2 plot
-#
+# read in the data file, and create plot4
 
-#load the data
-mainData <- read.table("./household_power_consumption.txt",header = TRUE, sep = ";", na.strings = c("?", ""))
+#read the data
+mainData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = c("?", ""))
 
-#combine date and time in a single column for graphing
+# combine the date and time for plotting
+#mainData$DateTime <- as.POSIXct(paste(mainData$Date, mainData$Time), format="%d/%m/%Y %H:%M:%S")
 mainData$DateTime <- paste(mainData$Date, mainData$Time)
 mainData$DateTime <- strptime(mainData$DateTime, format = "%d/%m/%Y %H:%M:%S")
 
-#data range: Feb 1st 2007 - Feb 2nd 2007
-filteredData <- subset(mainData, mainData$DateTime < ("2007-02-03 00:00:00") & mainData$DateTime > ("2007-02-01 00:00:00"))
 
-#convert global active power to numeric for the graph
+#convert the date for filtering
+mainData$Date <- as.Date(mainData$Date, "%d/%m/%Y")
+
+#filter the data
+filteredData <- mainData[mainData$Date == as.Date("1/2/2007", "%d/%m/%Y") | mainData$Date == as.Date("2/2/2007", "%d/%m/%Y"),]
+
+#convert the column to numeric for plotting purposes
 filteredData$Global_active_power <- as.numeric(filteredData$Global_active_power)
+ 
+#set up the plotting grid
+par(mfrow = c(2,2))
 
-#generate the plots
-
-#create a 2x2 area for the 4 plots with a white bakground
-par(mfrow = c(2,2), bg = "white")
+#plot the data
 with(filteredData, {
-  #Global_active_power
-  plot(filteredData$DateTime, filteredData$Global_active_power,type="l", bg = "white", ylab="Global Active Power (kilowatts)", xlab= "")
+  #first plot, global active power
+  plot(DateTime, Global_active_power, type = "l", bg="white", ylab="Global Active Power (kilowats)", xlab = "")
   
-  #Voltage
-  plot(filteredData$DateTime, filteredData$Voltage,type="l", bg = "white", cex= 0.5, ylab="Voltage", xlab= "datetime")
+  #second plot, voltage
+  plot(DateTime, Voltage, type = "l", bg="white", ylab="Voltage", xlab = "datetime")
   
-  #Energy sub metering multivariate plot
-  plot(filteredData$DateTime, filteredData$Sub_metering_1,type="l", col = "black", bg = "white", ylab="Energy sub metering", xlab= "")
-  lines(filteredData$DateTime, filteredData$Sub_metering_2,type="l", col = "red", bg = "white", ylab="Energy sub metering", xlab= "")
-  lines(filteredData$DateTime, filteredData$Sub_metering_3,type="l", col = "blue", bg = "white", ylab="Energy sub metering", xlab= "")
-  legend("topright", lty = 1,  bg = "transparent", cex = 0.8, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"))
+  #third plot, sub metering
+  plot(DateTime, Sub_metering_1,type="l", col = "black", ylab="Energy sub metering", xlab= "", bg = "white")
+  lines(DateTime, Sub_metering_2, col="red", ylab="Energy sub metering", xlab = "")
+  lines(DateTime, Sub_metering_3, col="blue", ylab="Energy sub metering", xlab = "")
+  legend("topright", lty = 1, cex = 0.5, bg = "transparent", box.col = "transparent", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   
-  #Global reactive power
-  plot(filteredData$DateTime, filteredData$Global_reactive_power,type="l", ylab="Global_reactive_power", xlab= "datetime", bg = "white")
+  #forth plot global reactive power
+  plot(DateTime, Global_reactive_power, type = "l", bg="white", ylab="Global_reactive_power", xlab = "datetime")
   
-  #save the graph and close the device
-  dev.copy(png, filename = "plot4.png", width = 480, height = 480, units = "px")
+  dev.copy(png, filename = "plot4.png", units = "px", width = 480, height = 480, pointsize = 12)
   dev.off()
+  
 })
+
